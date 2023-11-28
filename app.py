7688c32f8dc712977aed0db70c6befe88b5f4cf5
Ivan Lapickij <ivan.lapickij@gmail.com>
2023-11-28 11:23:53 +0000

    Implemented CRUD
    Software is working as expected , no errors found .

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ db = SQLAlchemy(app)
 class members(db.Model):
     id = db.Column('member_id', db.Integer, primary_key=True)
     name = db.Column(db.String(100))
+    gender = db.Column(db.String(100))
     membershipYears = db.Column(db.Integer)
     phone = db.Column(db.Integer)
     status = db.Column(db.String(100))
@@ -17,8 +18,9 @@ class members(db.Model):
     favoriteGame = db.Column(db.String(150))
     highestScore = db.Column(db.Integer)
 
-    def __init__(self, name, membershipYears, phone, status, gamesWon, favoriteGame, highestScore):
+    def __init__(self, name, gender, membershipYears, phone, status, gamesWon, favoriteGame, highestScore):
         self.name = name
+        self.gender = gender
         self.membershipYears = membershipYears
         self.phone = phone
         self.status = status
@@ -28,7 +30,7 @@ class members(db.Model):
 
 db.create_all()
 
-global_member = members('A', 1, 911, 'master', 200, 'Citadels', 2055)
+global_member = members('Garry', 'male', 1, 911, 'master', 200, 'Citadels', 2055)
 
 @app.route('/', methods=['GET', 'POST'])
 def opening2():
@@ -109,16 +111,18 @@ def update2():
         else:
             updateName = request.form['name']
             member = db.session.query(members).filter(members.name == updateName).first()
-            member.city = request.form['city']
-            member.addr = request.form['addr']
-            member.age = request.form['age']
+            member.gender = request.form['gender']  # Update this line
+            member.status = request.form['status']
+            member.membershipYears = request.form['membershipYears']
+            # Update other fields as needed
             db.session.commit()
 
             print('Record was successfully updated')
             return redirect(url_for('show_all'))
     searchMember = global_member
 
-    return render_template('UpdateMembers.html', student=searchMember)
+    return render_template('UpdateMembers.html', member=searchMember)
+
 
 @app.route('/increment_age2', methods=['GET', 'POST'])
 def increment_age2():
@@ -129,18 +133,20 @@ def increment_age2():
         else:
             updateName = request.form['name']
             member = db.session.query(members).filter(members.name == updateName).first()
-            age = int(request.form['age'])
-            member.age = age + 1
+            membershipYears = int(request.form['membershipYears'])
+            member.membershipYears = membershipYears + 1
             db.session.commit()
             return redirect(url_for('show_all'))
 
     searchMember = global_member
-    return render_template('IncrementMembership.html', student=searchMember)
+    return render_template('IncrementMembership.html', member=searchMember)
 
 @app.route('/show_all', methods=['GET', 'POST'])
 def show_all():
-    all_data = members.query.all()  # Query all members from the database
-    return render_template('ReadMembers.html', message='test', students=all_data)
+    all_data = members.query.all()
+    print("All data retrieved:", all_data)
+    return render_template('ReadMembers.html', message='test', members=all_data)
+
 
 
 @app.route('/initial_table_data', methods=['GET', 'POST'])
@@ -148,16 +154,16 @@ def initial_table_data():
     if request.method == 'POST':
         print('here')
         members_data = [
-            ('Ivan', 1, 911, 'master', 200, 'Citadels', 2055),
-            ('Elena', 2, 922, 'beginner', 150, 'Chess', 1900),
-            ('John', 3, 933, 'advanced', 300, 'Monopoly', 1800),
-            ('Sophia', 1, 944, 'master', 250, 'Risk', 2100),
-            ('Michael', 2, 955, 'beginner', 180, 'Catan', 1950),
-            ('Olivia', 3, 966, 'advanced', 280, 'Ticket to Ride', 2000),
-            ('William', 1, 977, 'master', 220, 'Scrabble', 1900),
-            ('Emma', 2, 988, 'beginner', 160, 'Uno', 1800),
-            ('Alexander', 3, 999, 'advanced', 320, 'Poker', 2050),
-            ('Ava', 1, 1000, 'master', 240, 'Codenames', 1950)
+            ('Ivan','Male', 1, 911, 'master', 200, 'Citadels', 2055),
+            ('Elena','Female', 2, 922, 'beginner', 150, 'Chess', 1900),
+            ('John','Male', 3, 933, 'advanced', 300, 'Monopoly', 1800),
+            ('Sophia','Female', 1, 944, 'master', 250, 'Risk', 2100),
+            ('Michael','Male', 2, 955, 'beginner', 180, 'Catan', 1950),
+            ('Olivia','Female', 3, 966, 'advanced', 280, 'Ticket to Ride', 2000),
+            ('William','Male', 1, 977, 'master', 220, 'Scrabble', 1900),
+            ('Emma','Female', 2, 988, 'beginner', 160, 'Uno', 1800),
+            ('Alexander','Male', 3, 999, 'advanced', 320, 'Poker', 2050),
+            ('Ava','Female', 1, 1000, 'master', 240, 'Codenames', 1950)
         ]
         # Create instances of members and add them to the database session
         for data in members_data:
@@ -166,31 +172,47 @@ def initial_table_data():
         db.session.commit()
         flash('Initial Table Data Added')
 
+        # Print the contents of the database for debugging
+    all_data = members.query.all()
+    print("All data in the database:", all_data)
+
     return render_template('InitialData.html')
 
 @app.route('/new', methods=['GET', 'POST'])
 def new():
     if request.method == 'POST':
-        # Check if 'name' and 'age' keys exist in the form data
-        if not request.form.get('name') or not request.form.get('age'):
-            flash('Please enter both name and age', 'error')
-        else:
-            # Retrieve values from form data
-            name = request.form['name']
-            age = request.form['age']
+        # Collect data from the form
+        name = request.form.get('name')
+        gender = request.form.get('gender')
+        membership_years = request.form.get('membershipYears')
+        phone = request.form.get('phone')
+        status = request.form.get('status')
+        games_won = request.form.get('gamesWon')
+        favorite_game = request.form.get('favoriteGame')
+        highest_score = request.form.get('highestScore')
 
-            # Create a new member instance
-            member = members(name=name, age=age)
+        # Create a new members object with the collected data
+        member = members(
+            name=name,
+            gender=gender,
+            membershipYears=membership_years,
+            phone=phone,
+            status=status,
+            gamesWon=games_won,
+            favoriteGame=favorite_game,
+            highestScore=highest_score
+        )
 
-            # Add the member to the database session
-            db.session.add(member)
-            db.session.commit()
+        # Add the new member to the session and commit the changes
+        db.session.add(member)
+        db.session.commit()
 
-            flash('Member added successfully')
+        flash('Member added successfully')  # Optionally, provide feedback to the user
 
-            return redirect(url_for('show_all'))
+        return redirect(url_for('show_all'))
 
     return render_template('CreateMembers.html')
+
 
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
